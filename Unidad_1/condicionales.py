6c5e11d3cf213759c6a67dd3ecfe6ab48adcf3be
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2023-01-10 09:43:21 +0100

    descomentar codigo

--- Unidad_1/condicionales.py
+++ Unidad_1/condicionales.py
@@ -1,30 +1,30 @@
-# #sintaxis basica
-# nota = int(input("Introduce la nota del alumno:"))
+#sintaxis basica
+nota = int(input("Introduce la nota del alumno:"))
 
-# if nota < 5:
-#     print('Suspenso.')
-#     if nota < 3:
-#         print('Vaya caca')
-# else:
-#     print('Aprobado como mínino')
+if nota < 5:
+    print('Suspenso.')
+    if nota < 3:
+        print('Vaya caca')
+else:
+    print('Aprobado como mínino')
 
-# #No hay switch en python -- uso de ELIF
+#No hay switch en python -- uso de ELIF
 
-# edad = int(input("Introduce la edad:"))
+edad = int(input("Introduce la edad:"))
 
-# if edad < 18:
-#     print('No pueedes pasar')
-# elif edad >= 18:
-#     print('Pasa hasta la seccion 1')
-# elif edad >= 20:
-#     print('Pasa hasta la seccion 1')
+if edad < 18:
+    print('No pueedes pasar')
+elif edad >= 18:
+    print('Pasa hasta la seccion 1')
+elif edad >= 20:
+    print('Pasa hasta la seccion 1')
 
-# ## concatenacion de operadores de comparacion
+## concatenacion de operadores de comparacion
 
-# if 0 < edad < 100:
-#     print('Edad correcta')
-# else:
-#     print('Edad incorrecta')
+if 0 < edad < 100:
+    print('Edad correcta')
+else:
+    print('Edad incorrecta')
 
 #operador in para comprobar si un valor esta dentro de una tupla
 
